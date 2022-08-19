bc3017da605ad8d25bf0495ef749f40dae1d4f75
awadh <awadh.hassan@student.moringaschool.com>
2022-08-19 12:10:49 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-const employee ={ name:"sam", streetAddress:"dd"
 
+const employee = {
+    name:"sam", streetAddress:"11 Broadway",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    for (const key in employee){
+        delete employee [key];
+    }
+    employee.name = "Sam";
+    let anotherEmployee = {
+        ...employee ,
+        streetAddress:"11 Broadway"
+    };
+    return anotherEmployee;
 }
-function updateEmployeeWithKeyAndValue(employee, key, value){
-    const newEmployee ={...employee};
-    newEmployee[key]= value ;
-     return newEmployee;
- }
-
-function destructivelyDeleteFromEmployeeByKey(employee, key){
-    delete employee[key];
-    return employee[key];
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    for (const key in employee) {
+        delete employee [key];
+    }
+    employee.name = "Sam";
+  employee.streetAddress = "12 Broadway";
+  return employee;
 }
-function  deleteFromEmployeeByKey(employee, key){
-    Object.keys(employee,key);
-    
+function deleteFromEmployeeByKey(){
+    for (const key in employee) {
+        delete employee [key];
+    }
+    employee.name = "Sam";
+    let anotherEmployee = {
+        ...employee
+    }
+    delete anotherEmployee.name
+    return anotherEmployee
+}
+function destructivelyDeleteFromEmployeeByKey() {
+    for (const key in employee) {
+        delete employee [key];
+    }
+    employee.name = "Sam";
+    let anotherEmployee = delete employee.name;
+    return employee
 }
